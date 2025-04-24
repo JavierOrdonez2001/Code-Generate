9ef8600cbc01be50d4a287c527be1f6002be8ea2
Javier Ordoñez <javier.ordonez.barra@gmail.com>
2025-04-24 15:06:06 -0400

    diseño modificado en /login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,15 @@
+function Login() {
+
+    
 
 
-
-function Login(){
-
-    return(
-        <div className="flex justify-center items-center min-h-screen">
-        <form className="bg-gray-800 p-8 rounded-2xl shadow-md w-full max-w-sm">
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen text-white space-y-6">
+        <h1 className="text-3xl font-bold text-blue-500 animate-pulse">
+          ¡Bienvenido a TechStorePlus!
+        </h1>
+  
+        <form className="bg-gray-800 p-8 rounded-2xl shadow-md w-full max-w-sm fade-in-up">
           <h2 className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</h2>
   
           <div className="mb-4">
@@ -28,14 +32,14 @@ function Login(){
   
           <button
             type="submit"
-            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-200"
+            className="w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-200 cursor-pointer"
           >
             Iniciar sesión
           </button>
         </form>
       </div>
     );
-}
-
-
-export default Login
+  }
+  
+  export default Login;
+  
